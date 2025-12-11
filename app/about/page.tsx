0c128f72f6feb14a3b6bb81d3e69470a4938b821
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 12:46:49 +0530

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function about() {
+  return (
+    <div className="flex min-h-screen items-center justify-center m-3 text-xl">
+      <h2>
+        About HnCC HnCC or Hackathon and Coding Club is the official club of BIT
+        Sindri that has the motto to instill a coding culture, collaborate, and
+        arrange events relevant to Open Source, Graphics and Game Development,
+        Web Development, App Development, and many other topics. Many students
+        at the institute are doing well, but they can do even more if they get
+        proper guidance. These students invariably maintain contact with a
+        select lot of elite, wise seniors who act as mentors or friends. The
+        purpose of HnCC is to provide an official platform for these efforts to
+        encourage them by officially endorsing them and helping them surmount
+        the hostel boundaries for a healthier and fruitful network.
+      </h2>
+    </div>
+  );
+}
