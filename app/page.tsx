c9e2c8771e042ec8c4be5ab5f1b9918f035c0001
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 12:40:30 +0530

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col  items-center justify-center font-sans bg-[ #ff1e00]">
-      <div className="flex-col md:flex mt-40">
+      <div className="flex-col md:flex ">
         <div className="flex-col md:flex items-center justify-center">
           <h1 className="text-xl pt-4 m-2">
             TechSpire is for the fresher who are really want to do something....
@@ -13,14 +13,14 @@ export default function Home() {
             asperiores quod. Quasi.
           </h1>
           <button className=" px-3 py-2 my-4 bg-[#3259E8] rounded-md w-full text-center ">
-            <a href="/login">Join Now ...</a>
+            <a href="/login">Join The Club</a>
           </button>
         </div>
         <div>
           <img
             src="./hero_image.jpeg"
             alt="hero_image"
-            className="h-100 w-400 rounded-full"
+            className="h-100 w-400 md:h-200 md:w-800 rounded-full"
           />
         </div>
       </div>
