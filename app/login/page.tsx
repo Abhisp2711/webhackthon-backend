f961bed587e8029c40c9f6c6ae27bcfc15e4249b
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 12:39:03 +0530

    created login page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h2>This is the login page we will update soon...</h2>
+    </div>
+  );
+}
