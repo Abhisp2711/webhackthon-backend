ad5b678faeb389cc17ce1f816a234ad517b1d1bb
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 14:32:26 +0530

    created project

---
+++ app/project/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function project() {
+  return (
+    <div className="text-white flex items-center justify-center min-h-screen">
+      <h2 className="text-xl">
+        Thank you for your patience we will update soon...
+      </h2>
+    </div>
+  );
+}
