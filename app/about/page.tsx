eadd3809485911e6b50d18dbd18f93018ddf4b0c
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 13:24:46 +0530

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,18 +2,19 @@
 
 export default function about() {
   return (
-    <div className="flex min-h-screen items-center justify-center m-3 text-xl">
+    <div className="flex min-h-screen items-center justify-center text-xl text-white">
       <h2>
-        About HnCC HnCC or Hackathon and Coding Club is the official club of BIT
-        Sindri that has the motto to instill a coding culture, collaborate, and
-        arrange events relevant to Open Source, Graphics and Game Development,
-        Web Development, App Development, and many other topics. Many students
-        at the institute are doing well, but they can do even more if they get
-        proper guidance. These students invariably maintain contact with a
-        select lot of elite, wise seniors who act as mentors or friends. The
-        purpose of HnCC is to provide an official platform for these efforts to
-        encourage them by officially endorsing them and helping them surmount
-        the hostel boundaries for a healthier and fruitful network.
+        About TechSpire or Hackathon and Coding Club is the official club of
+        Chaibase Engineering college that has the motto to instill a coding
+        culture, collaborate, and arrange events relevant to Open Source,
+        Graphics and Game Development, Web Development, App Development, and
+        many other topics. Many students at the institute are doing well, but
+        they can do even more if they get proper guidance. These students
+        invariably maintain contact with a select lot of elite, wise seniors who
+        act as mentors or friends. The purpose of TechSpire is to provide an
+        official platform for these efforts to encourage them by officially
+        endorsing them and helping them surmount the hostel boundaries for a
+        healthier and fruitful network.
       </h2>
     </div>
   );
