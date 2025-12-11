b4dbf5108489c5a4b0ef1f8e170b15a0ec6a6e39
Abhisp2711 <abhisp2711@gmail.com>
2025-12-11 14:26:41 +0530

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col  items-center justify-center font-sans bg-[ #ff1e00]">
-      <div className="flex-col md:flex ">
-        <div className="flex-col md:flex items-center justify-center">
-          <h1 className="text-xl pt-4 m-2 text-white">
-            TechSpire is for the fresher who are really want to do something....
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-            repellendus eius nesciunt, nam amet necessitatibus perspiciatis nemo
-            cum dolorem recusandae ipsa, omnis adipisci itaque qui nulla
-            asperiores quod. Quasi.
-          </h1>
-          <button className=" px-3 py-2 my-4 bg-[#3259E8] rounded-md w-full text-center ">
-            <a href="/login">Join The Club</a>
-          </button>
+      {/* container */}
+      <div className="p-10">
+        {/* hero  */}
+        <div className="flex gap-5 flex-wrap-reverse">
+          <div>
+            <h1 className="text-3xl text-white  ">TehcSpire Club</h1>
+            <div className="w-full h-0.5 bg-white mb-4"></div>
+            <p className=" text-xl text-white">
+              TechSpire is for the fresher who are really want to do
+              something.... Lorem ipsum dolor sit, amet consectetur adipisicing
+              elit. Quo magni repellendus eius nesciunt, nam amet necessitatibus
+              perspiciatis nemo cum dolorem recusandae ipsa, omnis adipisci
+              itaque qui nulla asperiores quod. Quasi.
+            </p>
+          </div>
+          <div className="">
+            <img src="./hero_image.jpeg" alt="" />
+          </div>
         </div>
         <div>
-          <img
-            src="./hero_image.jpeg"
-            alt="hero_image"
-            className="h-100 w-400 md:h-200 md:w-800 rounded-full"
-          />
+          <div>
+            <button className=" px-3 py-2 my-4 bg-[#3259E8] rounded-md w-full text-center ">
+              <a href="/login">Apply now</a>
+            </button>
+            <div className=" flex items-center justify-center">
+              <input className="bg-white p-2 rounded-md mr-3" type="email" />
+              <button className=" px-3 py-2 my-4 bg-[#3259E8] rounded-md text-center ">
+                <a href="/login">Subscribe</a>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
